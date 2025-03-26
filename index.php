26c46179c5f022e50e6cd6a1bc9d9fd702feda6e
TranMinhHuy2003 <bosstran1212@gmail.com>
2025-03-26 22:54:34 +0700

    Using isset and empty function in PHP

--- index.php
+++ index.php
@@ -7,32 +7,31 @@
 </head>
 <body>
   <form action="index.php" method="post">
-    <label for="">country:</label>
-    <input type="text" name="country" id=""><br>
-    <input type="submit" value="submit">
+    <label for="">username:</label>
+    <input type="text" name="username" id="">
+    <label for="">password:</label>
+    <input type="password" name="password" id="">
+    <input type="submit" name="login" value="Login"><br>
   </form>
 </body>
 </html>
 <?php
-  $capitals = array("USA" => "Washington D.C.",
-                    "Japan" => "Kyoto",
-                    "South Korea" => "Seoul",
-                    "India" => "New Dehli");
-  $countries = array_keys($capitals);
-  $country = $_POST["country"];
-  if (!in_array($country, $countries)) {
-    echo "The country is not available";
-  } else {
-    $capital = $capitals[$country];
-    echo "The capital of {$country} is {$capital}";
+  //isset(): return TRUE if a variable is declared and not null
+  //empty(): return TRUE if a variable is not declared, false, null, ""
+  
+  foreach($_POST as $key => $value) {
+    echo "{$key} = {$value} <br>";
   }
 
-  //$capitals["USA"] = "Los Vegas";
-  // $capitals["China"] = "Beijing";
-  // array_pop($capitals);
-  // array_shift($capitals);
-  // $keys = array_keys($capitals);
-  // $values = array_values($capitals);\
-  // $capitals = array_flip($capitals);
-  // $capitals = array_reverse($capitals);
+  if(isset($_POST["login"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    if(empty($username)) {
+      echo "username is missing";
+    } elseif(empty($password)) {
+      echo "password is missing";
+    } else {
+      echo "Hello {$username}";
+    }
+  } 
 ?>
