c482eeed552b4c80ec337fb07869191924779ba4
TranMinhHuy2003 <bosstran1212@gmail.com>
2025-03-26 14:18:41 +0700

    math functions in PHP

--- index.php
+++ index.php
@@ -6,21 +6,49 @@
   <title>Document</title>
 </head>
 <body>
-  <form action="index.php" method="get">
-    <label for="">quantity: </label><br>
-    <input type="text" name="quantity" id=""><br>
-    <input type="submit" value="total">
+  <form action="index.php" method="post">
+    <label for="">radius: </label><br>
+    <input type="text" name="radius" id=""><br>
+    <input type="submit" value="calculate">
   </form>
+  <br>
 </body>
 </html>
-<?php 
-  $item = "pizza";
-  $price = 5.99;
-  $total = null;
-  $quantity = $_GET["quantity"];
+<?php
+  $r = $_POST["radius"];
+  $c = null;
+  $s = null;
+  $v = null;
 
-  $total = $price * $quantity;
+  $c = 2*pi()*$r;
+  $c = round($c, 2);
 
-  echo "You have ordered {$quantity} x {$item}/s <Br>";
-  echo "Your total is: \${$total}";
+  $s = pi() * pow($r, 2);
+  $s = round($s, 2);
+
+  $v = 4/3 * pi() * pow($r, 3);
+  $v = round($v, 2);
+
+  echo "Circumference: {$c}cm <br>";
+  echo "Area: {$s}cm^2 <br>";
+  echo "Volume: {$v}cm^3";
+
+  // $x = $_POST["x"];
+  // $y = $_POST["y"];
+  // $z = $_POST["z"];
+  // $total = null;
+  
+  // //$total = abs($x);
+  // //$total = round($x); -3 -> 3
+  // //$total = floor($x); all round down
+  // //$total = ceil($x); all round down
+  // //$total = sqrt($x);
+  // //$total = pow($x, $y);
+  // //$total = max($x, $y, $z);
+  // //$total = min($x, $y, $z);
+  // //$total = pi();
+  // $total = rand(1, 6);
+
+
+  // echo $total;
 ?>
