e0151f51ebb10cd533655f76a6f78b3a8a1ee011
TranMinhHuy2003 <bosstran1212@gmail.com>
2025-03-27 13:00:07 +0700

    Using sanitize and validate in PHP

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="index.php" method="post">
+    username: <br>
+    <input type="text" name="username" id=""><br>
+    age: <br>
+    <input type="text" name="age" id=""><br>
+    email: <br>
+    <input type="text" name="email" id=""><br>
+    <input type="submit" name="login" value="Login">
+  </form>
+</body>
+</html>
 <?php
-  $username = "Minh Huy";
-  $phone = "092-8976-3173";
-  // $username = strtolower($username);
-  // $username = strtoupper($username);
-  // $username = trim($username); clear blank
-  // $username = str_pad($username, 20, "0");
-  // $phone = str_replace("-", "", $phone);
-  // $username = strrev($username);
-  // $username = str_shuffle($username);
-  // $equal = strcmp($username, "Huy");
-  // $count = strlen($username);
-  // $index = strpos($phone, "-");
-  // $firstname = substr($username, 0, 4);
-  // $lastname = substr($username, 5);
-
-  //$username_arr = explode(" ", $username); string to array
-  // foreach($username_arr as $name) {
-  //   echo $name . "<br>";
-  // }
-
-  // $username_arr = ["Minh", "Huy"]; array to string
-  // $username = implode(" ", $username_arr);
-
-  echo $username;
+//Sanitize: chuẩn hóa dữ liệu
+//Validate: kiểm tra dữ liệu có hợp lệ hay không, không hợp lệ thì empty
+  if(isset($_POST["login"])) {
+    $age = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
+    $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+    if(empty($email)) {
+      echo "That email wasn't valid";
+    } else {
+      echo "Your email is {$email}";
+    }
+  }
 ?>
