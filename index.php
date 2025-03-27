4716003607ba7f59683e21bb5148a1c832bca465
TranMinhHuy2003 <bosstran1212@gmail.com>
2025-03-27 23:05:37 +0700

    Select in mysql by PHP

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
   include("database.php");
 
-  $username = "Huy Tran";
-  $password = "pizza32423";
-  $hash = password_hash($password, PASSWORD_DEFAULT);
+  $sql = "SELECT * from users";
+  $result = mysqli_query($conn, $sql);
 
-  $sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash')";
-  try {
-    mysqli_query($conn, $sql);
-    echo "User is now registered <br>";
-  }
-  catch(mysqli_sql_exception) {
-    echo "Could not register user";
+  if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+      echo $row["id"] . "<br>";
+      echo $row["user"] . "<br>";
+      echo $row["reg_date"] . "<br>";
+    };
+  } else {
+    echo "No user found";
   }
 
   mysqli_close($conn);
