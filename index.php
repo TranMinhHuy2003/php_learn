b7f5f47ed393bdc17e147fa6ae914786256d18cf
TranMinhHuy2003 <bosstran1212@gmail.com>
2025-03-27 23:31:25 +0700

    Making a registration form by PHP

--- index.php
+++ index.php
@@ -1,18 +1,48 @@
 <?php
   include("database.php");
-
-  $sql = "SELECT * from users";
-  $result = mysqli_query($conn, $sql);
-
-  if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
-      echo $row["id"] . "<br>";
-      echo $row["user"] . "<br>";
-      echo $row["reg_date"] . "<br>";
-    };
-  } else {
-    echo "No user found";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>", method="post">
+    <h2>Welcome to Facebook</h2>
+    username: <br>
+    <input type="text" name="username" id=""> <br>
+    password: <br>
+    <input type="text" name="password" id=""> <br>
+    <input type="submit" value="register" name="submit">
+  </form>
+</body>
+</html>
+<?php
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+  
+    if(empty($username)) {
+      echo "Please enter username";
+    }
+    elseif(empty($password)) {
+      echo "Please enter password";
+    }
+    else {
+      $hash = password_hash($password, PASSWORD_DEFAULT);
+      $sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash')";
+      try {
+        mysqli_query($conn, $sql);
+        echo "You are now registered";
+      }
+      catch(mysqli_sql_exception) {
+        echo "That username is taken";
+      }
+    }
   }
+
 
   mysqli_close($conn);
 ?>
